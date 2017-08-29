1dd1e3e2837cb0262a2b6e2971544dd768102837
Joel Rahman <joel@flowmatters.com.au>
2017-08-29 15:03:02 +1000

    Basic regression test scripts

--- dsed/compare_results.py
+++ dsed/compare_results.py
@@ -1,6 +1,86 @@
 
+import os
+from glob import glob
+import pandas as pd
+import numpy as np
 
 # 
+THRESHOLD = 1e-2
+THRESHOLD_FRAC = 1e-3
+
+def findResultsFiles(path):
+    entries = glob(os.path.join(path,'*'))
+    result = []
+    
+    for entry in entries:
+        if os.path.isdir(entry):
+            result += findResultsFiles(entry)
+        else:
+            result.append(entry)
+    return result
+                  
+def findFiles(resultsSet,base=''):
+    return findResultsFiles(os.path.join(base,resultsSet))
+
+def fileComparison(lhs,rhs):
+    if not lhs.endswith('.csv'):
+        return None# ['SKIPPED']
+
+    dsL = pd.read_csv(lhs,error_bad_lines=False)
+    if not os.path.exists(rhs):
+        return 'file missing'
+
+    dsR = pd.read_csv(rhs,error_bad_lines=False)
+    oCols = list(dsL.columns[np.where(dsL.dtypes=='object')])
+    if len(oCols)==len(dsL.columns):
+        return None
+    
+    dsL = dsL.sort_index()#inplace=True)
+    dsR = dsR.sort_index()#inplace=True)
+
+    if len(dsL.index) != len(dsR.index) or np.any(dsL.index!=dsR.index):
+        return 'mismatched index'
+
+    if len(oCols):
+        dsL.sort(oCols,inplace=True)
+        dsR.sort(oCols,inplace=True)
+        if set(dsL.index)==set(range(len(dsL.index))):
+            dsL['new_index'] = list(range(len(dsL.index)))
+            dsL = dsL.set_index('new_index')
+            dsR['new_index'] = list(range(len(dsL.index)))
+            dsR = dsR.set_index('new_index')
+    else:
+        dsL.sort(inplace=True)
+        dsR.sort(inplace=True)
+
+    if len(dsL.columns) != len(dsR.columns) or np.any(dsL.columns != dsR.columns):
+        return 'mismatched columns', set(dsL.columns)-set(dsR.columns)
+
+    bothNan = np.logical_and(pd.isnull(dsL),pd.isnull(dsR))
+    result = np.logical_and(dsL!=dsR,np.logical_not(bothNan))
+#    rows = result.transpose().any()
+    numericCols = list(set(dsL.columns)-set(oCols))
+    numericL = dsL[numericCols]
+    numericR = dsR[numericCols]
+
+    delta = numericL-numericR
+    relative = delta / numericL
+    rows = np.logical_and(np.abs(delta)>THRESHOLD,np.abs(relative)>THRESHOLD_FRAC).transpose().any()
+    if rows.any():
+        result = dsL[rows].copy()
+        for col in numericCols:
+            result['RHS_'+col] = dsR[rows][col]
+            result['DIFF_'+col] = delta[rows][col]
+            result['REL_DIFF_'+col] = relative[rows][col]
+            
+        return (result,dsL[rows],dsR[rows])
+    return None
+
+def test_comparison(lhs,rhs):
+    try:
+        return fileComparison(lhs,rhs)
+    except Exception as e:
+        return str(e)
 
 def compare(resultsSet1,resultsSet2):
     print('Comparing %s %s'%(resultsSet1, resultsSet2),end='')
@@ -8,8 +88,8 @@ def compare(resultsSet1,resultsSet2):
     files2 = [f.replace(resultsSet1,resultsSet2) for f in files1]
     pairs = zip(files1,files2)
    
-    tmp = {lhs.split('/')[-1]:fileComparison(lhs,rhs) for lhs,rhs in pairs}
+    tmp = {lhs.split('/')[-1]:test_comparison(lhs,rhs) for lhs,rhs in pairs}
     print(' DONE')
-    return {key:val for key,val in tmp.items() if not val is None}
+    return {key:val for key,val in tmp.items() if not val is None}, [k for k,v in tmp.items() if v is None]
 
     
--- dsed/regression_test.py
+++ dsed/regression_test.py
@@ -1,5 +1,85 @@
 '''
 Functionality to support regression testing of dynamic sednet
 '''
+import os
+import sys
+import json
 
-def simulation_test(project_file)
+import tempfile
+import shutil
+from veneer.manage import create_command_line, start, kill_all_now
+import veneer
+from .compare_results import compare
+import pandas as pd
+
+RUN_NAME='regression_test'
+
+def simulation_test(project_file,
+                    expected_results_folder,
+                    veneer_path,
+                    source_version='4.1.1',
+                    port=44444,
+                    temp_dir=None):
+    processes=None
+    delete_after=False
+
+    if not temp_dir:
+        delete_after=True
+        temp_dir = tempfile.mkdtemp('_dsed_test')
+
+    try:
+        veneer_cmd_path = create_command_line(veneer_path,source_version,dest=os.path.join(temp_dir,'veneer_cmd'),force=False)
+        processes,ports = start(project_file,veneer_exe=veneer_cmd_path,
+                                overwrite_plugins=True,ports=port,
+                                remote=False,script=True,debug=True)
+
+        v = veneer.Veneer(port=ports[0])
+        output_path = os.path.join(temp_dir,'test_outputs')
+        if os.path.exists(output_path):
+            shutil.rmtree(output_path)
+        result, run_url = v.run_model(ModelOutputPath=output_path,runName=RUN_NAME,IsRegressionTestRun=False)
+        assert result==302
+
+        scen_name = v.model.get('scenario.Name')
+        full_results_path = os.path.join(output_path,scen_name,RUN_NAME)
+        errors, check_ok = compare(expected_results_folder,full_results_path)
+        if len(errors):
+            print("==== ERRORS ====")
+            for fn,error in errors.items():
+                print("* %s"%fn)
+                print(error)
+                print("-"*40)
+            assert False
+        assert len(check_ok)
+
+    finally:
+        if processes:
+            kill_all_now(processes)
+
+        if delete_after:
+            shutil.rmtree(temp_dir)
+
+if __name__=='__main__':
+    test_fn = sys.argv[1]
+    veneer_path = sys.argv[2]
+    source_version = '4.1.1' if len(sys.argv)<4 else sys.argv[3]
+
+    tests = pd.read_csv(test_fn)
+    wd = os.getcwd()
+    results = {}
+    for ix in range(len(tests)):
+        row = tests.irow(ix)
+        os.chdir(row.Folder)
+        try:
+            simulation_test(row.ProjectFn,
+                            row.ExpectedResults,
+                            veneer_path,
+                            source_version)
+            print("SUCCESS: " + row.Folder)
+            results[row.Folder]='SUCCESS'
+        except Exception as e:
+            print('FAILED: '+row.Folder)
+            results[row.Folder]='FAILURE: %s'%str(e)
+        finally:
+            os.chdir(wd)
+    print(json.dumps(results))
