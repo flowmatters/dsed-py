dc9a02821b05809af1293a08a8f67770cd5c48f1
Joel Rahman <joel@flowmatters.com.au>
2020-10-07 15:39:54 +1100

    Logic for identifying regulated links in model comparison

--- dsed/testing/migration.py
+++ dsed/testing/migration.py
@@ -4,7 +4,9 @@ from openwater.discovery import discover, set_exe_path
 from openwater.results import OpenwaterResults
 from openwater.template import ModelFile
 import dsed.migrate as migrate
+from veneer.general import _extend_network
 from veneer.utils import split_network
+from veneer.extensions import _feature_id
 from assertpy import assert_that
 import os
 import json
@@ -61,17 +63,27 @@ class OpenwaterImplementation(object):
         self.fn = fn
         self.ow_model_fn = fn + '.h5'
         self.meta = json.load(open(fn+'.meta.json'))
-        self.nodes = gpd.read_file(fn+'.nodes.json')
-        self.links = gpd.read_file(fn+'.links.json')
-        self.catchments = gpd.read_file(fn+'.catchments.json')
+        self.init_network(fn)
+
         self.ow_results_fn = fn+'_outputs.h5'
         self.dates = pd.date_range(self.meta['start'], self.meta['end'])
         self.open_files()
 
+    def init_network(self,fn):
+        self.nodes = gpd.read_file(fn+'.nodes.json')
+        self.links = gpd.read_file(fn+'.links.json')
+        self.catchments = gpd.read_file(fn+'.catchments.json')
+        raw = [json.load(open(fn+'.'+c+'.json','r')) for c in ['nodes','links','catchments']]
+        self.network = {
+            'type':'FeatureCollection',
+            'crs':raw[0]['crs'],
+            'features':sum([r['features'] for r in raw],[])
+        }
+        self.network = _extend_network(self.network)
+
     def run_model(self):
         self.model.run(self.dates, self.ow_results_fn, overwrite=True)
         self.open_files()
-
 
     def open_files(self):
         _ensure_uncompressed(self.ow_model_fn)
@@ -82,6 +94,24 @@ class OpenwaterImplementation(object):
                                         self.dates)
         self.model = ModelFile(self.ow_model_fn)
 
+    def regulated_links(self):
+        network = self.network
+        outlet_nodes = network.outlet_nodes()
+        outlets = [n['properties']['name'] for n in outlet_nodes]
+        network.partition(outlets,'outlet')
+        storages = network['features'].find_by_icon('/resources/StorageNodeModel')
+
+        impacted_by_storage = []
+        for s in storages:
+            outlet = s['properties']['outlet']
+            outlet_id = _feature_id(network['features'].find_by_name(outlet)[0])
+            impacted_by_storage += network.path_between(s,outlet_id)
+
+        ids = set([_feature_id(f) for f in impacted_by_storage])
+        network_df = network.as_dataframe()
+        impacted_by_storage = network_df[network_df['id'].isin(ids)]
+        links_downstream_storage = [l.replace('link for catchment ','') for l in impacted_by_storage[impacted_by_storage.feature_type=='link'].name]
+        return links_downstream_storage
 
 class RegressionTest(object):
     def __init__(self, expected, actual):
