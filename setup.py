3a4442fce24333159b6e63c8c50433900e36f97a
Joel Rahman <joel@flowmatters.com.au>
2020-11-19 12:23:04 +1100

    Missed package in setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         name='dsed-py',
         version=version,
         description="Support for running dynamic sednet simulations",
-        packages=["dsed","dsed.testing"],
+        packages=["dsed","dsed.testing","dsed.migrate"],
         long_description=read("README.md"),
         classifiers=filter(None, classifiers.split("\n")),
         keywords='hydrology scripting dynamic sednet',
