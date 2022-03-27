e4b02501cc5c3b948a4ba5c7f930101fc7c4fb0f
Joel Rahman <joel@flowmatters.com.au>
2022-03-27 14:22:39 +1100

    Implement comparison using generic openwater class

--- dsed/migrate/compare.py
+++ dsed/migrate/compare.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 from dsed.const import *
+import openwater.examples.catchment_model_comparison as cmc
+from dsed import ow
 
 R_SQD_THRESHOLD_NUT=0.99
 SSQUARES_THRESHOLD=1
@@ -37,6 +39,15 @@ def problem_counts(df,*args):
 
     return pd.DataFrame(grouped_lengths(df,problems,nonzero,*args))
 
+def _arg_parser():
+  parser = cmc._arg_parser()
+  return parser
+
+def dsed_model_comparison(m,source_files,ow_dir,component=None,con=None):
+  return cmc.model_comparison(ow.OpenwaterDynamicSednetResults,m,source_files,ow_dir,component,con)
+
 if __name__=='__main__':
-    print('Compare')
+  import veneer.extract_config as ec
+  args = ec._parsed_args(_arg_parser())
+  cmc.compare_main(dsed_model_comparison,**args)
 
