a140bfdc4c393d98fa8b5f963f19b543b444cad7
Joel Rahman <joel@flowmatters.com.au>
2022-04-07 09:26:50 +1000

    Fix imports

--- dsed/migrate/__init__.py
+++ dsed/migrate/__init__.py
@@ -1,10 +1,10 @@
 '''
 Functionality to migrate a Source Dynamic Sednet model to Openwater (with limitations)
-
-
 '''
-from .build import *
-from .compare import *
-from .extract import *
-from .util import *
+
+from . import build
+from . import compare
+from . import extract
+from . import util
+
 
